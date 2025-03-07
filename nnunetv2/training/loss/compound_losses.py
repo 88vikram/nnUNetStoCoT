inference based on 1st network. fixed bug in masking probabilities. alpha,beta in output directory now

nnunetv2/training/loss/compound_losses.py
@@ -53,7 +53,6 @@ class DC_and_CE_loss(nn.Module):
         
         ce_loss = self.ce(net_output, target[:, 0]) \
             if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
-
         result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
         
         return result
@@ -85,7 +84,7 @@ class DC_and_CE_StoCoT_loss(nn.Module):
 
 
 
-    def generate_stochastic_thresholds(self,x,alpha,beta,learning_rate):
+    def generate_stochastic_thresholds(self,x,alpha,beta,coteaching_schedule):
 
         samples=x.shape[0]
         if len(x.shape)==4:
@@ -93,14 +92,15 @@ class DC_and_CE_StoCoT_loss(nn.Module):
         elif len(x.shape)==5:
             dist_shape = (samples,8,16,16)
         
-        if learning_rate != 0:
+        stochastic_thresholds = torch.zeros((1,), dtype=torch.float32, device=x.device).expand(tuple(dist_shape))
+        if coteaching_schedule != 0:
             stochastic_thresholds = torch.from_numpy(
-                learning_rate * self.rng.beta(a=alpha, b=beta, size=dist_shape).astype(np.float32)).cuda()
+                coteaching_schedule * self.rng.beta(a=alpha, b=beta, size=dist_shape).astype(np.float32)).cuda()
         
         return stochastic_thresholds
         # generate thresholds based on beta pdf.
 
-    def forward(self, net_output1: torch.Tensor,net_output2: torch.Tensor, target: torch.Tensor, learning_rate: float):
+    def forward(self, net_output1: torch.Tensor,net_output2: torch.Tensor, target: torch.Tensor, current_epoch: float):
         """
         target must be b, c, x, y(, z) with c=1
         :param net_output:
@@ -119,7 +119,16 @@ class DC_and_CE_StoCoT_loss(nn.Module):
             target_dice = target
             mask = None
 
-        stochastic_thresholds = self.generate_stochastic_thresholds(net_output1,self.alpha,self.beta,learning_rate)
+        n_coteach_start = 5
+        if current_epoch<n_coteach_start:
+            #no stochastic co-teaching
+            coteaching_schedule=0
+        elif current_epoch<(2*n_coteach_start):
+            overall_schedule=np.linspace(0, 1, n_coteach_start+1)
+            coteaching_schedule=overall_schedule[current_epoch-n_coteach_start]
+        else:
+            coteaching_schedule=1
+        stochastic_thresholds = self.generate_stochastic_thresholds(net_output1,self.alpha,self.beta,coteaching_schedule)
 
         if self.weight_dice != 0:
             dc_loss1, dc_loss2 = self.dc(net_output1, net_output2, target_dice,stochastic_thresholds,loss_mask=mask)
@@ -138,7 +147,6 @@ class DC_and_CE_StoCoT_loss(nn.Module):
         
         #ce_loss2 = self.ce(net_output2, target[:, 0]) \
         #    if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
-
         result1 = self.weight_ce * ce_loss1 + self.weight_dice * dc_loss1
         result2 = self.weight_ce * ce_loss2 + self.weight_dice * dc_loss2
         

nnunetv2/training/loss/dice.py
@@ -4,6 +4,7 @@ import torch
 from nnunetv2.utilities.ddp_allgather import AllGatherGrad
 from torch import nn
 import numpy as np 
+import torch.nn.functional as F
 
 
 class SoftDiceLoss(nn.Module):
@@ -92,11 +93,14 @@ class StoCoTSoftDiceLoss(nn.Module):
 
         return final_repeated_tensor
 
-    def mask_probabilities(self,x,final_stochastic_thresholds):        
+    def mask_probabilities(self,x,y,final_stochastic_thresholds):        
 
-        mask = torch.ones((1,), dtype=torch.float32, device=x.device).expand(tuple(x.shape))
-        mask = torch.gt(x, final_stochastic_thresholds).type(torch.cuda.FloatTensor)
-        
+        class_dim=1
+        probabilities = F.softmax(x, dim=class_dim)
+        class_probabilities = torch.gather(probabilities,class_dim,y.long())
+        mask = torch.ge(class_probabilities, final_stochastic_thresholds[:,0]).type(torch.cuda.FloatTensor)
+        mask = mask.expand(x.shape)
+
         return mask
         # generate thresholds based on beta pdf.
 
@@ -112,15 +116,17 @@ class StoCoTSoftDiceLoss(nn.Module):
             x2 = self.apply_nonlin(x2)
 
         final_stochastic_thresholds=self.repeat_and_truncate(stochastic_thresholds,x1.shape,1)
-        stocot_mask1 = self.mask_probabilities(x1,final_stochastic_thresholds)
-        stocot_mask2 = self.mask_probabilities(x2,final_stochastic_thresholds)
+        stocot_mask1 = self.mask_probabilities(x1,y,final_stochastic_thresholds)
+        stocot_mask2 = self.mask_probabilities(x2,y,final_stochastic_thresholds)
 
         fraction_reject_1 = (1. - (stocot_mask1.sum() / torch.prod(torch.tensor(stocot_mask1.shape)))).item()
         fraction_reject_2 = (1. - (stocot_mask2.sum() / torch.prod(torch.tensor(stocot_mask2.shape)))).item()
-        #print('Rejection rates', fraction_reject_1, fraction_reject_2, x1.shape)
+
+#        if fraction_reject_1>0:
+#            print('Rejection rates', fraction_reject_1, fraction_reject_2, x1.shape)
 
         if loss_mask is not None:
-            print('Current implementation ignores the variable loss_mask. Please rectify this before using this further')
+            raise ValueError('Current implementation ignores the variable loss_mask. Please rectify this before using the code further')
             
         tp, fp, fn, _ = stocot_get_tp_fp_fn_tn(x1, y, axes, stocot_mask2, False)
         tp2, fp2, fn2, _ = stocot_get_tp_fp_fn_tn(x2, y, axes, stocot_mask1, False)
@@ -157,7 +163,6 @@ class StoCoTSoftDiceLoss(nn.Module):
         
         dc = dc.mean()
         dc2 = dc2.mean()
-
         return -dc, -dc2
 
 

nnunetv2/training/loss/robust_ce_loss.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn, Tensor
 import numpy as np
-
+import torch.nn.functional as F
 
 class RobustCrossEntropyLoss(nn.CrossEntropyLoss):
     """
@@ -13,8 +13,9 @@ class RobustCrossEntropyLoss(nn.CrossEntropyLoss):
         if target.ndim == input.ndim:
             assert target.shape[1] == 1
             target = target[:, 0]
-        
-        return super().forward(input, target.long())
+        ce_loss = super().forward(input, target.long())
+        print(ce_loss)
+        return ce_loss
     
 
 class StoCoTRobustCrossEntropyLoss(nn.CrossEntropyLoss):
@@ -47,11 +48,14 @@ class StoCoTRobustCrossEntropyLoss(nn.CrossEntropyLoss):
 
         return final_repeated_tensor
 
-    def mask_probabilities(self,x,final_stochastic_thresholds):        
+    def mask_probabilities(self,x,y,final_stochastic_thresholds):        
 
-        mask = torch.ones((1,), dtype=torch.float32, device=x.device).expand(tuple(x.shape))
-        mask = torch.gt(x, final_stochastic_thresholds).type(torch.cuda.FloatTensor)
-        
+        class_dim=1
+        probabilities = F.softmax(x, dim=class_dim)
+        class_probabilities = torch.gather(probabilities,class_dim,y.unsqueeze(class_dim).long())
+        mask = torch.ge(class_probabilities, final_stochastic_thresholds[:,0]).type(torch.cuda.FloatTensor)
+        mask = mask.expand(x.shape)
+
         return mask
 
     def forward(self, input1: Tensor,input2: Tensor, target: Tensor,stochastic_thresholds:Tensor):
@@ -60,9 +64,9 @@ class StoCoTRobustCrossEntropyLoss(nn.CrossEntropyLoss):
             target = target[:, 0]
 
         final_stochastic_thresholds=self.repeat_and_truncate(stochastic_thresholds,input1.shape,1)
-        stocot_mask1 = self.mask_probabilities(input1,final_stochastic_thresholds).to(torch.bool)
-        stocot_mask2 = self.mask_probabilities(input2,final_stochastic_thresholds).to(torch.bool)
-
+        stocot_mask1 = self.mask_probabilities(input1,target,final_stochastic_thresholds).to(torch.bool)
+        stocot_mask2 = self.mask_probabilities(input2,target,final_stochastic_thresholds).to(torch.bool)
+        
         loss_object=nn.CrossEntropyLoss(reduction='none')
 
         if input1.ndim != target.ndim:
@@ -75,12 +79,13 @@ class StoCoTRobustCrossEntropyLoss(nn.CrossEntropyLoss):
             y_onehot = torch.zeros(input1.shape, device=input1.device, dtype=torch.bool)
             y_onehot.scatter_(1, target.long(), 1)
 
-        loss1=loss_object.forward(input1[stocot_mask2], y_onehot[stocot_mask2].float())
-        loss2=loss_object.forward(input2[stocot_mask1], y_onehot[stocot_mask1].float())
+
+        loss1=(loss_object.forward(input1, y_onehot.float()))*stocot_mask2
+        loss2=(loss_object.forward(input2, y_onehot.float()))*stocot_mask1
 
         ce_loss1=loss1.sum() / stocot_mask2.sum()
         ce_loss2=loss2.sum() / stocot_mask1.sum()
-
+        
         return ce_loss1,ce_loss2
 
 

nnunetv2/training/nnUNetTrainer/nnUNetTrainerStoCoT.py
@@ -71,7 +71,7 @@ from nnunetv2.utilities.plans_handling.plans_handler import PlansManager
 
 class nnUNetTrainerStoCoT(object):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
+                 device: torch.device = torch.device('cuda'), alpha: float = 33, beta: float = 2.1):
         # From https://grugbrain.dev/. Worth a read ya big brains ;-)
 
         # apex predator of grug is complexity
@@ -91,12 +91,9 @@ class nnUNetTrainerStoCoT(object):
 
         self.is_ddp = dist.is_available() and dist.is_initialized()
         self.local_rank = 0 if not self.is_ddp else dist.get_rank()
-
         self.device = device
-
-        # parameters of the beta distribution
-        self.alpha=32 # default values
-        self.beta=2
+        self.alpha=alpha
+        self.beta=beta
 
         # print what device we are using
         if self.is_ddp:  # implicitly it's clear that we use cuda in this case
@@ -130,7 +127,7 @@ class nnUNetTrainerStoCoT(object):
         self.preprocessed_dataset_folder_base = join(nnUNet_preprocessed, self.plans_manager.dataset_name) \
             if nnUNet_preprocessed is not None else None
         self.output_folder_base = join(nnUNet_results, self.plans_manager.dataset_name,
-                                       self.__class__.__name__ + '__' + self.plans_manager.plans_name + "__" + configuration) \
+                                       self.__class__.__name__ + '__' + self.plans_manager.plans_name + "__" + configuration+"__"+f"{self.alpha:.1f}"+'_'+f"{self.beta:.1f}") \
             if nnUNet_results is not None else None
         self.output_folder = join(self.output_folder_base, f'fold_{fold}')
 
@@ -153,7 +150,7 @@ class nnUNetTrainerStoCoT(object):
         self.oversample_foreground_percent = 0.33
         self.num_iterations_per_epoch = 250
         self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 1000
+        self.num_epochs = 500
         self.current_epoch = 0
         self.enable_deep_supervision = True
 
@@ -1027,8 +1024,7 @@ class nnUNetTrainerStoCoT(object):
             output1 = self.network1(data)
             output2 = self.network2(data)
             # del data
-            l,l2 = self.loss(output1, output2, target,len(output1)*[self.optimizer1.param_groups[0]['lr']])
-
+            l,l2 = self.loss(output1, output2, target,len(output1)*[self.current_epoch])
         if self.grad_scaler1 is not None:
             self.grad_scaler1.scale(l).backward()
             self.grad_scaler1.unscale_(self.optimizer1)
@@ -1066,7 +1062,6 @@ class nnUNetTrainerStoCoT(object):
         else:
             loss_here = np.mean(outputs['loss1'])
             loss_here2 = np.mean(outputs['loss2'])
-
         self.logger.log('train_losses1', loss_here, self.current_epoch)
         self.logger.log('train_losses2', loss_here2, self.current_epoch)
 
@@ -1093,7 +1088,7 @@ class nnUNetTrainerStoCoT(object):
             output1 = self.network1(data)
             output2 = self.network2(data)
             del data
-            l, l2 = self.loss(output1, output2, target,len(output1)*[self.optimizer1.param_groups[0]['lr']])
+            l, l2 = self.loss(output1, output2, target,len(output1)*[self.current_epoch])
 
         # we only need the output with the highest output resolution (if DS enabled)
         if self.enable_deep_supervision: