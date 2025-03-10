bug fix in stocot loss computation

nnunetv2/training/loss/dice.py
@@ -97,7 +97,9 @@ class StoCoTSoftDiceLoss(nn.Module):
 
         class_dim=1
         class_probabilities = torch.gather(x,class_dim,y.long())
-        mask = torch.ge(class_probabilities, final_stochastic_thresholds[:,0]).type(torch.cuda.FloatTensor)
+        final_stochastic_thresholds=final_stochastic_thresholds[:,0].type(torch.cuda.FloatTensor)
+        final_stochastic_thresholds = final_stochastic_thresholds.unsqueeze(class_dim)
+        mask = torch.ge(class_probabilities, final_stochastic_thresholds)
         mask = mask.expand(x.shape)
 
         return mask

nnunetv2/training/loss/robust_ce_loss.py
@@ -52,8 +52,9 @@ class StoCoTRobustCrossEntropyLoss(nn.CrossEntropyLoss):
 
         class_dim=1
         class_probabilities = torch.gather(x,class_dim,y.unsqueeze(class_dim).long())
-        mask = torch.ge(class_probabilities, final_stochastic_thresholds[:,0]).type(torch.cuda.FloatTensor)
-        mask = mask.expand(x.shape)
+        final_stochastic_thresholds=final_stochastic_thresholds[:,0].type(torch.cuda.FloatTensor)
+        final_stochastic_thresholds = final_stochastic_thresholds.unsqueeze(class_dim)
+        mask = torch.ge(class_probabilities, final_stochastic_thresholds)
 
         return mask
 